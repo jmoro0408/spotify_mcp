d0ab064cec97c6d1e653133490b805ee31967105
James Moro <james.moro.11@gmail.com>
2025-04-21 13:11:29 -0300

    rename file

--- src/server.py
+++ src/server.py
@@ -4,14 +4,14 @@ from mcp.server.fastmcp import FastMCP
 from auth import SpotipyClient
 from utils import strip_uri
 
-# Create an MCP server
 mcp = FastMCP("Spotify_MCP")
+
+client = SpotipyClient()
 
 
 @mcp.resource("play://{artist}_{song}")  # how to name this?
 def get_uri_from_artist_song(artist: str, song: str) -> str | None:
-    auth = SpotipyClient()
-    sp = auth.sp
+    sp = client.sp
     query = f"artist:{artist} track:{song}"
     results = sp.search(q=query, type="track", limit=1)
     if results is None:
@@ -27,12 +27,11 @@ def get_uri_from_artist_song(artist: str, song: str) -> str | None:
 @mcp.tool(name="play_song_by_uri")
 def play_song_by_uri(uri: str) -> None:
     uri = strip_uri(uri)
-    auth = SpotipyClient()
-    sp = auth.sp
+
+    sp = client.sp
     sp.start_playback(uris=[f"spotify:track:{uri}"])
     return None
 
 
 if __name__ == "__main__":
-    # Initialize and run the server
     mcp.run(transport="stdio")
