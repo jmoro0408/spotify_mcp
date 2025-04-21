2b5be10f5749b62681c6e335786cdd97ea7ca0fb
James Moro <james.moro.11@gmail.com>
2025-04-21 18:39:32 -0300

    add get_currently_playing tracks

--- src/server.py
+++ src/server.py
@@ -1,19 +1,46 @@
 from mcp.server.fastmcp import FastMCP
 
 from resources import _get_uri_from_artist_song
-from tools import _play_song_by_uri
+from tools import _get_currently_playing, _play_song_by_artist, _play_song_by_uri
 
 mcp = FastMCP("Spotify_MCP")
 
+# Resources
 
-@mcp.resource("play://{artist}_{song}")
+
+@mcp.resource(
+    "play://{artist}_{song}",
+    name="get_uri_from_artist_song",
+    description="get unique spotify uri from artist and song",
+)
 def get_uri_from_artist_song(artist: str, song: str) -> str | None:
     return _get_uri_from_artist_song(artist, song)
 
 
-@mcp.tool(name="play_song_by_uri")
+# Tools
+
+
+@mcp.tool(
+    name="get_currently_playing",
+    description="get the currently playing artist and song",
+)
+def get_currently_playing() -> str | None:
+    return _get_currently_playing()
+
+
+@mcp.tool(
+    name="play_song_by_uri", description="play a song on spotify given its unique uri"
+)
 def play_song_by_uri(uri: str) -> bool:
     return _play_song_by_uri(uri)
+
+
+@mcp.tool(
+    name="play_song_by_artist_song",
+    description="play a song on spotify given its artist and song",
+)
+def play_song_by_artist(artist: str, song: str) -> bool:
+    return _play_song_by_artist(artist=artist, song=song)
 
 
 if __name__ == "__main__":
--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,5 @@
 from auth import client
+from resources import _get_uri_from_artist_song
 from utils import logger, strip_uri
 
 
@@ -10,4 +11,28 @@ def _play_song_by_uri(uri: str) -> bool:
         return True
     except Exception as e:
         logger.error(f"Failed to play song: {str(e)}")
+        raise
+
+
+def _play_song_by_artist(artist: str, song: str) -> bool:
+    song_uri = _get_uri_from_artist_song(artist, song)
+    if song_uri:
+        return _play_song_by_uri(song_uri)
+    else:
+        return False
+
+
+def _get_currently_playing() -> str:
+    try:
+        sp = client.sp
+        current_track = sp.currently_playing()
+
+        if current_track and current_track["is_playing"]:
+            track_name = current_track["item"]["name"]
+            artist_name = current_track["item"]["artists"][0]["name"]
+            return f"Now playing: {track_name} by {artist_name}"
+        else:
+            return "No track is currently playing."
+    except Exception as e:
+        logger.error(f"Failed to get currently playing: {str(e)}")
         raise
