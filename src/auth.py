38658ed73cad548150c1b421b6dd2e2d85e06cad
James Moro <james.moro.11@gmail.com>
2025-04-22 13:10:27 -0300

    change permissions to 777

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,4 @@
+import os
 import spotipy
 from dotenv import find_dotenv, load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
@@ -7,7 +8,10 @@ class SpotipyClient:
     def __init__(self):
         load_dotenv(find_dotenv())
         scope = "user-library-read,user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
-        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+        cache_path = os.path.join(os.path.expanduser("~"), ".spotify_mcp_cache")
+        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(scope=scope, cache_path=cache_path)
+        )
 
 
 client = SpotipyClient()
